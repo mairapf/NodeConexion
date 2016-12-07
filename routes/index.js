dd7286a0ce8459e64fe9f21ca61b45def6beeafb
maira <yaky_138@hotmail.com>
2016-12-07 17:33:30 -0500

    d

--- routes/index.js
+++ routes/index.js
@@ -138,4 +138,36 @@ router.post('/panel', (req, res, next) => {
   });
 });
 
+router.post('/art', (req, res, next) => {
+
+	res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  const results = [];
+  // Grab data from http request
+  const data = {dato: req.body.dato };
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    
+    const query = client.query('select * from articulo where codigo_articulo = $1',
+    [data.dato]);
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
 module.exports = router;
